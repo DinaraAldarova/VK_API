db225efd81f00057990021575b7257dba7f81276
Vadim <bakhtin_94@bk.ru>
2019-12-04 18:02:08 +0500

    Добавьте файлы проекта.

---
+++ FormAuth.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace VK_API
+{
+    public partial class FormAuth : Form
+    {
+        public FormAuth()
+        {
+            InitializeComponent();
+            InitBrowser();
+        }
+        public ChromiumWebBrowser browser;
+        public void InitBrowser()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser("https://oauth.vk.com/authorize?client_id=6620613&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,video,photos,offline&response_type=token&v=5.52");
+            this.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+            browser.
+        }
+        private void FormAuth_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VK_API
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            FormAuth formAuth = new FormAuth();
+            formAuth.ShowDialog();
+        }
+    }
+}
